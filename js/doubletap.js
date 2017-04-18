3e825300971e807494e38b62e26ae33d872c41d2
mint-cipher <cpumaster930@optonline.net>
2017-04-18 02:16:33 -0400

    next attempt...

--- js/doubletap.js
+++ js/doubletap.js
@@ -1,21 +1,28 @@
-(function($) {
-  var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
-  $.fn.nodoubletapzoom = function() {
-    if (IS_IOS)
-      $(this).bind('touchstart', function preventZoom(e) {
-        var t2 = e.timeStamp
-          , t1 = $(this).data('lastTouch') || t2
-          , dt = t2 - t1
-          , fingers = e.originalEvent.touches.length;
-        $(this).data('lastTouch', t2);
-        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+$(window).bind('touchend', function (e) {
+    e.preventDefault();
+    // Add your code here. 
+    $(this).click();
+    // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
+})
 
-        e.preventDefault(); // double tap - prevent the zoom
-        // also synthesize click events we just swallowed up
-        $(this).trigger('click').trigger('click');
-      });
-  };
-})(jQuery);
+//(function($) {
+//  var IS_IOS = /iphone|ipad/i.test(navigator.userAgent);
+//  $.fn.nodoubletapzoom = function() {
+//    if (IS_IOS)
+//      $(this).bind('touchstart', function preventZoom(e) {
+//        var t2 = e.timeStamp
+//          , t1 = $(this).data('lastTouch') || t2
+//          , dt = t2 - t1
+//          , fingers = e.originalEvent.touches.length;
+//        $(this).data('lastTouch', t2);
+//        if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+//
+//        e.preventDefault(); // double tap - prevent the zoom
+//        // also synthesize click events we just swallowed up
+//        $(this).trigger('click').trigger('click');
+//      });
+//  };
+//})(jQuery);
 
 //(function ($) {
 //    var count = 0;
