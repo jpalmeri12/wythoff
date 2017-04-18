648b30067695e70fc81a71a0e61f9757701a6fbd
mint-cipher <cpumaster930@optonline.net>
2017-04-18 02:28:59 -0400

    man

--- js/doubletap.js
+++ js/doubletap.js
@@ -2,7 +2,6 @@ $(window).bind('touchend', function (e) {
     e.preventDefault();
     // Add your code here. 
     e.currentTarget.click();
-    alert("hi");
     // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
 })
 
